f211e84000e6e1109b7b25980c38c3e98186e514
jan <janjancaranguian@gmail.com>
2025-09-04 11:01:47 +0800

    Finish the login

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,14 @@ import bcrypt from "bcrypt";
 import { pool } from './mysqlConnection.js'
 
 
+type User = {
+    Id: number;
+    UserName: string;
+    Email: string;
+    Salt: string;
+    Hash: string;
+}
+
 // remember: Every line of code is a potential vulnerability
 
 const app = express();
@@ -36,18 +44,22 @@ app.post("/users", async (req, res) => {
 });
 
 app.post("/users/login", async (req, res) => {
-    const query = 'SELECT Hash FROM Users WHERE UserName = ? OR Email = ? ;';
-    const [ rows ] = await pool.query(query, [req.body.userName, req.body.email]); // select the user first 
-    const user = null;
-    const hashedUserPassword = "";
-
-    if (user === null) {
+    const query = 'SELECT * FROM Users WHERE UserName = ? OR Email = ? ;';
+    const [ results ] = await pool.query(query, [req.body.userName, req.body.email]); // select the user first 
+    const data: User[] = JSON.parse(JSON.stringify(results));
+   
+    const passwordInput = req.body.password;
+    
+    
+    
+    if (data.length === 0 || data[0] === undefined) {
         res.status(404);
         res.send("User not found");
         return;
     }
     try {
-        if ( await bcrypt.compare(req.body.password, hashedUserPassword) ) {
+        const passwordDB = data[0].Hash;
+        if ( await bcrypt.compare(passwordInput, passwordDB) ) {
             res.send("Successfully logged in")
         } else {
             res.send("Wrong password. Try again.")
