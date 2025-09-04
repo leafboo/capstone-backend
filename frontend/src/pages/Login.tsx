bf716db7b49c85f8726d91fa583673a39b7e0515
Jan <110758056+leafboo@users.noreply.github.com>
2025-09-04 15:01:29 +0800

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
 
 function Login() {
-  
+  async function loginUser(event: FormData) {
+    const userName = event.get("userName");
+    const email = event.get("email");
+    const password = event.get("password");
+
+    const url = "http://localhost:3000/users/login";
+    
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userName: userName, email: email, password: password })
+      });
+      const result = await response.json();
+      console.log(result);
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <>
       <h2 className="font-bold text-[5rem]">Login</h2>
-      <form action="">
+      <form action={loginUser}>
         <label htmlFor="nameEmail">username or email</label> <br />
         <input id='nameEmail' type="text" className='border' /> <br />
         <label htmlFor="password">password</label> <br />
