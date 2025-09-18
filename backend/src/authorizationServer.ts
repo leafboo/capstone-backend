80d97be00a8220c6e5f5b678196c1d59f8a43541
Jan <110758056+leafboo@users.noreply.github.com>
2025-09-18 12:43:53 +0800

    Send the JWT refresh token as httpOnly cookie as well

--- backend/src/authorizationServer.ts
+++ backend/src/authorizationServer.ts
@@ -86,6 +86,7 @@ app.post("/login", async (req, res) => {
 
             // we need the 'secure' attribute set to 'true' to only use the cookies with HTTPS only
             res.cookie('jwt_access_token', accessToken, { httpOnly: true, sameSite: 'strict' }) // the sameSite attribute prevents CSRF attacks (it will only send the cookie if it originated from the same site)
+            res.cookie('jwt_refresh_token', refreshToken, { httpOnly: true, sameSite: 'strict' })
             res.json({ success: "true" });
         } else {
             res.send("Wrong password. Try again.")
