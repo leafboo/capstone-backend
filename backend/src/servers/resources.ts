f97886a3e0bcbf8413fe6d87c0dd88157c4f1b2b
jan <janjancaranguian@gmail.com>
2025-09-27 12:27:40 +0800

    Refactor POST /users endpoint

--- backend/src/servers/resources.ts
+++ backend/src/servers/resources.ts
@@ -25,6 +25,29 @@ app.get("/users", (req, res) => {
     res.send("User accounts")
 });
 
+
+app.post("/users", async (req, res) => {
+    const {password, userName, email} = req.body;
+
+    if (!password) {
+        return res.send("body should contain password and should not be empty");
+        
+    }
+    
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const query = 'INSERT INTO Users(UserName, Email, Salt, Hash) VALUES(?, ?, ?, ?);'
+        const result = await pool.query(query, [userName, email, salt, hashedPassword]);
+        res.status(201);
+        res.send(result);
+    } catch (err) {
+        console.error(err)
+        res.status(500);
+    }
+});
+
 // For getting the current user's details
 app.get("/users/me", authenticateToken, async (req, res) => {
     const userId = res.locals.user.sub;
@@ -71,26 +94,6 @@ app.get("/workspaces/:id", authenticateToken, async (req, res) => {
 });
 
 
-app.post("/users", async (req, res) => {
-
-    if (!req.body.password) {
-        return res.send("body should contain password and should not be empty");
-        
-    }
-    
-    try {
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-        const query = 'INSERT INTO Users(UserName, Email, Salt, Hash) VALUES(?, ?, ?, ?);'
-        const result = await pool.query(query, [req.body.userName, req.body.email, salt, hashedPassword]);
-        res.status(201);
-        res.send(result);
-    } catch (err) {
-        console.error(err)
-        res.status(500);
-    }
-});
 
 
 
