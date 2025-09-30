8ea7aff04d4565f04329de6571c46e7f9a7169f6
jan <janjancaranguian@gmail.com>
2025-09-30 20:32:46 +0800

    Remove the /paperReferences endpoint

--- backend/src/servers/resources.ts
+++ backend/src/servers/resources.ts
@@ -125,12 +125,12 @@ app.delete("/workspaces/:id", authenticateToken, async (req, res) => {
 
 // ---------------------------------------/researchPapers-----------------------------------------------------------------------------
 app.post("/workspaces/:id/researchPapers", authenticateToken, async (req, res) => {
-    const { title, yearOfPublication, keywords, abstract, methods, findings} = req.body;
+    const { title, yearOfPublication, keywords, abstract, methods, findings, apa, ieee} = req.body;
     const { id } = req.params;
      
-    const query = `INSERT INTO ResearchPapers (Title, YearOfPublication, Keywords, Abstract, Methods, Findings, WorkspaceId)
+    const query = `INSERT INTO ResearchPapers (Title, YearOfPublication, Keywords, Abstract, Methods, Findings, apa, ieee, WorkspaceId)
                    VALUES (?, ?, ?, ?, ?, ?)`;
-    const [ results ] = await pool.query(query, [title, yearOfPublication, keywords, abstract, methods, findings, id]);
+    const [ results ] = await pool.query(query, [title, yearOfPublication, keywords, abstract, methods, findings, apa, ieee, id]);
     const researchPaperId = JSON.parse(JSON.stringify(results)).insertId;
 
     res.status(201).json({message: "Research paper successfully added", researchPaperId: researchPaperId});
@@ -157,40 +157,8 @@ app.delete("/researchPapers/:id", authenticateToken, async (req, res) => {
 	res.status(200).json({message: "Successfully deleted research paper"});
 });
 
-// ---------------------------------------/paperReferences-----------------------------------------------------------------------------
 
 
-app.get("/workspaces/:id/paperReferences", authenticateToken, async (req, res) => {
-	const { id } = req.params;
-
-	const query = 'SELECT * FROM PaperReferences WHERE WorkspaceId = ?';
-	const [ results ] = await pool.query(query, [id]);
-	console.log(results);
-
-	res.status(200).json({message: "Paper References results should be displayed here"});
-});
-
-app.post("/workspaces/:id/paperReferences", authenticateToken, async (req, res) => {
-	const { apa, ieee } = req.body;
-	const { id } = req.params;
-
-	const query = `INSERT INTO PaperReferences (APA, IEEE, WorkspaceId);
-		       VALUES (?, ?, ?)`;
-	const [ results ] = await pool.query(query, [apa, ieee, id]);
-	console.log(results);
-
-	res.status(201).json({message: "Reference successfully added "});
-});
-
-app.delete("/paperReferences/:id", authenticateToken, async (req, res) => {
-	const { id } = req.params;
-
-	const query = 'DELETE FROM PaperReferences WHERE Id = ?';
-	const [ results ] = await pool.query(query, [id]);
-	console.log(results);
-
-	res.status(200).json({message: "Reference successfully deleted"});
-});
 
 
 
