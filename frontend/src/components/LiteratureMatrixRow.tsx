46470df10e3e6c3f7d0b2add64bca970a2e54c9e
jan <janjancaranguian@gmail.com>
2025-10-09 17:23:54 +0800

    Table cells' value will now go to the default value prior to editing when the user clicks out of the cell

--- frontend/src/components/LiteratureMatrixRow.tsx
+++ frontend/src/components/LiteratureMatrixRow.tsx
@@ -16,7 +16,6 @@ type litMatrixRowType = {
 export default function LiteratureMatrixRow({handleDeleteResearchPaper, Id, Title, Authors, PublicationYear, Abstract, Keywords, Methods, Findings}: litMatrixRowType) {
 
     function updateTableCell(event: React.KeyboardEvent<HTMLTableCellElement>) {
-        
         if (event.key === 'Enter') {
             event.preventDefault();
 
@@ -29,18 +28,52 @@ export default function LiteratureMatrixRow({handleDeleteResearchPaper, Id, Titl
         
     }
 
+
+    function handleResetInnerHtml(event: React.FocusEvent<HTMLTableCellElement>) {
+        let data = "";
+        switch(event.currentTarget.id) {
+            case "Title": 
+                data = Title;
+                break;
+            case "Authors": 
+                data = Authors;
+                break;
+            case "PublicationYear":
+                data = PublicationYear;
+                break;
+            case "Abstract": 
+                data = Abstract;
+                break;
+            case "Keywords":
+                data = Keywords;
+                break;
+            case "Methods":
+                data = Methods;
+                break;
+            case "Findings":
+                data = Findings;
+                break;
+            default:
+                console.warn("Could not identify the ResearchPapers column you edited");
+
+        }
+        if (data.length != 0) {
+            event.currentTarget.innerHTML = data;
+        }
+    }
+    
     
 
     return (
         <>
             <tr>
-                <td id="Title" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell}>{Title}</td>
-                <td id="Authors" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell}>{Authors}</td>
-                <td id="PublicationYear" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell}>{PublicationYear}</td>
-                <td id="Keywords" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell}>{Keywords}</td>
-                <td id="Abstract" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell} >{Abstract}</td>
-                <td id="Methods" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell}>{Methods}</td>
-                <td id="Findings" className="p-[1rem] border-[1px]" contentEditable onKeyDown={updateTableCell}>{Findings}</td>
+                <td id="Title" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml}>{Title}</td>
+                <td id="Authors" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml}>{Authors}</td>
+                <td id="PublicationYear" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml}>{PublicationYear}</td>
+                <td id="Keywords" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml}>{Keywords}</td>
+                <td id="Abstract" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml} >{Abstract}</td>
+                <td id="Methods" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml}>{Methods}</td>
+                <td id="Findings" className="p-[1rem] border-[1px]" contentEditable suppressContentEditableWarning onKeyDown={updateTableCell} onBlur={handleResetInnerHtml}>{Findings}</td>
                 <td className="p-[1rem] border-[1px] cursor-pointer hover:bg-red-700 hover:border-red-700 hover:text-white" onClick={()=> { handleDeleteResearchPaper(Id) }}>x</td>
             </tr>
         </>
